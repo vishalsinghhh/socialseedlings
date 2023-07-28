b747939c650aad9d599a86f47bf466e7277f06dd
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-28 11:42:16 +0530

    minor changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../../images/logo.svg";
-import "../components.css";
+import styles from "./navbar.module.css"
 
 const Navbar = () => {
   return (
     <div>
-      <div className="logoMain">
+      <div className={styles.logoMain}>
         <Image src={logo} alt="logo" />
         <div>SocialSeedlings</div>
-        <div className="midTagLine">Fostering Societal Change</div>
-        <div className="endTagLine">Eat. Socialize. Sleep <br/>xRepeat</div>
+        <div className={styles.midTagLine}>Fostering Societal Change</div>
+        <div className={styles.endTagLine}>Eat. Socialize. Sleep <br/>xRepeat</div>
       </div>
     </div>
   );
