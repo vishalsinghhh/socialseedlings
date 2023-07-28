6d8a669d0fc716f2947647cee7699e1acf693af0
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-28 18:56:12 +0530

    few changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:[
-            
-        ]
+        domains:['images.unsplash.com']
     }
 }
 
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./Card.module.css";
+import Image from "next/image";
 
 const Card = (randomPhoto) => {
-    console.log(randomPhoto);
+  console.log(randomPhoto.randomPhoto);
   return (
-    <div>Card</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.userProfile}>
+        <Image
+          src={randomPhoto.randomPhoto.user.profile_image.medium}
+          width={50}
+          height={50}
+          alt="logo"
+        />
+        <div>{randomPhoto.randomPhoto.user.instagram_username}</div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
