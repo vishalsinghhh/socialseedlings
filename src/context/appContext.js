4f1eb60e477f4a9efcdec5163993d010aa1b50d0
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-30 02:07:59 +0530

    context api

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -10,12 +10,22 @@ import {
   GET_RANDOM_PHOTO_BEGIN,
   GET_RANDOM_PHOTO_SUCCESS,
   GET_RANDOM_PHOTO_ERROR,
+  GET_USER_INFO_BEGIN,
+  GET_USER_INFO_SUCCESS,
+  GET_USER_INFO_ERROR,
+  GET_USER_PHOTOS_BEGIN,
+  GET_USER_PHOTOS_SUCCESS,
+  GET_USER_PHOTOS_ERROR,
 } from "./actions";
 
 const initialState = {
   mode: "dark",
   randomPhotos: [],
-  isLoading:false
+  isLoading: false,
+  isUserLoading: false,
+  isUserPhotosLoading: false,
+  userInfo: null,
+  userPhotos:null
 };
 
 const AppContext = React.createContext();
@@ -27,23 +37,72 @@ const AppProvider = ({ children }) => {
   };
 
   const getRandomPhoto = async () => {
-    dispatch({type:GET_RANDOM_PHOTO_BEGIN})
+    dispatch({ type: GET_RANDOM_PHOTO_BEGIN });
     try {
       const response = await axios.get(
         `https://api.unsplash.com/photos/random?client_id=ja4_MxOihhzXv8nFowzO0ggv_uUHZkTvbopJcMm7NX8`
       );
-      dispatch({type:GET_RANDOM_PHOTO_SUCCESS, payload:{data:response.data}})
+      dispatch({
+        type: GET_RANDOM_PHOTO_SUCCESS,
+        payload: { data: response.data },
+      });
     } catch (error) {
       console.log(error);
       dispatch({
-          type:GET_RANDOM_PHOTO_ERROR,
-          payload:{msg:'Oops! Something went wrong'}
-      })
+        type: GET_RANDOM_PHOTO_ERROR,
+        payload: { msg: "Oops! Something went wrong" },
+      });
+    }
+  };
+
+  const getUserInfo = async (userName) => {
+    dispatch({ type: GET_USER_INFO_BEGIN });
+    try {
+      const response = await axios.get(
+        `https://api.unsplash.com/users/${userName}?client_id=ja4_MxOihhzXv8nFowzO0ggv_uUHZkTvbopJcMm7NX8`
+      );
+      dispatch({
+        type: GET_USER_INFO_SUCCESS,
+        payload: { data: response.data },
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: GET_USER_INFO_ERROR,
+        payload: { msg: "Oops! Something went wrong" },
+      });
+    }
+  };
+
+  const getUserPhotos = async () => {
+    dispatch({ type: GET_USER_PHOTOS_BEGIN });
+    try {
+      const response = await axios.get(
+        `https://api.unsplash.com/users/${userName}/photos?client_id=ja4_MxOihhzXv8nFowzO0ggv_uUHZkTvbopJcMm7NX8`
+      );
+      dispatch({
+        type: GET_USER_PHOTOS_SUCCESS,
+        payload: { data: response.data },
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: GET_USER_PHOTOS_ERROR,
+        payload: { msg: "Oops! Something went wrong" },
+      });
     }
   };
 
   return (
-    <AppContext.Provider value={{ ...state, changeTheme, getRandomPhoto }}>
+    <AppContext.Provider
+      value={{
+        ...state,
+        changeTheme,
+        getRandomPhoto,
+        getUserInfo,
+        getUserPhotos,
+      }}
+    >
       <div className={`theme ${state.mode}`}>{children}</div>
     </AppContext.Provider>
   );
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -3,6 +3,12 @@ import {
   GET_RANDOM_PHOTO_BEGIN,
   GET_RANDOM_PHOTO_SUCCESS,
   GET_RANDOM_PHOTO_ERROR,
+  GET_USER_INFO_BEGIN,
+  GET_USER_INFO_SUCCESS,
+  GET_USER_INFO_ERROR,
+  GET_USER_PHOTOS_BEGIN,
+  GET_USER_PHOTOS_SUCCESS,
+  GET_USER_PHOTOS_ERROR,
 } from "./actions";
 
 const reducer = (state, action) => {
@@ -29,6 +35,44 @@ const reducer = (state, action) => {
     return {
       ...state,
       isLoading: false,
+    };
+  }
+  if (action.type === GET_USER_INFO_BEGIN) {
+    return {
+      ...state,
+      isUserLoading: true,
+    };
+  }
+  if (action.type === GET_USER_INFO_SUCCESS) {
+    return {
+      ...state,
+      isUserLoading: false,
+      userInfo:action.payload.data
+    };
+  }
+  if (action.type === GET_USER_INFO_ERROR) {
+    return {
+      ...state,
+      isUserLoading: false,
+    };
+  }
+  if (action.type === GET_USER_PHOTOS_BEGIN) {
+    return {
+      ...state,
+      isUserPhotosLoading: true,
+    };
+  }
+  if (action.type === GET_USER_PHOTOS_SUCCESS) {
+    return {
+      ...state,
+      isUserPhotosLoading: false,
+      userPhotos:action.payload.data
+    };
+  }
+  if (action.type === GET_USER_PHOTOS_ERROR) {
+    return {
+      ...state,
+      isUserPhotosLoading: false,
     };
   }
 };
