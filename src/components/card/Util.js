f2b0cba9a81ba400bdff07e20f249963d23ed87e
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-29 19:39:02 +0530

    new changes

--- src/components/card/Util.js
+++ src/components/card/Util.js
@@ -6,59 +6,48 @@ const Util = ({ name }) => {
     <div className="MainScroll">
       <div className="scroll text1">
         <div>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
         </div>
       </div>
       <div className="scroll text2">
         <div>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
         </div>
       </div>
       <div className="scroll text3">
         <div>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
         </div>
       </div>
       <div className="scroll text4">
         <div>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
-          <span>{" "}</span>
-          {name}
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
         </div>
       </div>
       <div className="scroll text5">
         <div>
-          {name}
-          <span> </span>
-          {name}
-          <span> </span>
-          {name}
-          <span> </span>
-          {name}
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
+          <span>{name}</span>
         </div>
-
       </div>
     </div>
   );
