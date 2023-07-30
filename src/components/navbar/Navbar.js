e368656b1fb509681c0cbd582c3c0bda9c926df7
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-31 05:28:57 +0530

    navbar fix

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,31 +1,27 @@
-"use client"
 import React from "react";
 import Image from "next/image";
 import logo from "../../images/logo.svg";
 import styles from "./navbar.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import Link from "next/link";
-import { useAppContext } from "@/context/appContext";
 
 const Navbar = () => {
-  const {mode} = useAppContext()
   return (
     <div>
       <div className={styles.logoMain}>
         <Link href={"/"} className={styles.myLink}>
-          <div className={mode==='dark'?`${styles.mainLogo}`:`${styles.mainLogo1}`}>
+          <div className={styles.mainLogo}>
             <Image src={logo} alt="logo" className={styles.logo} />
             <div>SocialSeedlings</div>
           </div>
         </Link>
-        <div className={mode==='dark'?`${styles.midTagLine}`:`${styles.midTagLine1}`}>
+        <div className={styles.midTagLine}>
           Fostering Societal Change
           <div>
             <DarkModeToggle className={styles.toggle} />
           </div>
         </div>
-        
-        <div className={mode==='dark'?`${styles.endTagLine}`:`${styles.endTagLine1}`}>
+        <div className={styles.endTagLine}>
           Eat. Socialize. Sleep <br />
           xRepeat
         </div>
