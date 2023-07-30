553f794f339ad507f417925ed91f8b4978532b9b
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-30 23:00:15 +0530

    loader

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { useEffect, useRef, useCallback } from "react";
 import { useAppContext } from "@/context/appContext";
 import Card from "@/components/card/Card";
+import { InfinitySpin } from "react-loader-spinner";
 
 export default function Home() {
   const { getRandomPhoto, randomPhotos, isLoading } = useAppContext();
@@ -31,22 +32,25 @@ export default function Home() {
       {randomPhotos.map((item, index) => {
         if (randomPhotos.length === index + 1) {
           return (
-            <div className={index===0 ?styles.container1:styles.container2}>
-            
+            <div
+              className={index === 0 ? styles.container1 : styles.container2}
+            >
               <Card randomPhoto={item} key={item.id} />
-              {(isLoading && randomPhotos.length<10) && <div>loading...</div>}
+              {isLoading && randomPhotos.length < 10 && (
+                <div>
+                  <InfinitySpin width="200" color="#fff" />
+                </div>
+              )}
               <div ref={lastBookElementRef}></div>
             </div>
           );
-        } 
-        else if (index === 0){
+        } else if (index === 0) {
           return (
             <div className={styles.container1}>
               <Card randomPhoto={item} key={item.id} />
             </div>
           );
-        }
-        else {
+        } else {
           return (
             <div className={styles.container2}>
               <Card randomPhoto={item} key={item.id} />
--- src/components/photoGrid/PhotosGrid.js
+++ src/components/photoGrid/PhotosGrid.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./photoGrid.module.css";
+import { RxCross1 } from "react-icons/rx";
+import CardUtil from "../card/CardUtil";
 
 const PhotosGrid = (item) => {
   console.log(item.photo.blur_hash);
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className={styles.mainPhotos}>
-      <Image src={item?.photo?.urls.regular} width={280} height={300} />
-      <div className={styles.block}>{item?.photo.alt_description}</div>
+    <div>
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <div
+            className={styles.cross}
+            onClick={() => {
+              setModalOpen(false);
+            }}
+          >
+            <RxCross1 />
+          </div>
+          <CardUtil
+            username={item?.photo?.user.username}
+            profileImg={item?.photo?.user.profile_image.medium}
+            location={item?.photo?.user.location}
+            createdAt={item?.photo?.created_at}
+            img={item?.photo?.urls.regular}
+            likes={item?.photo?.likes}
+          />
+        </div>
+      )}
+      <div className={styles.mainPhotos} onClick={() => setModalOpen(true)}>
+        <Image src={item?.photo?.urls.regular} width={280} height={300} />
+        <div className={styles.block}>{item?.photo.alt_description}</div>
+      </div>
     </div>
   );
 };
