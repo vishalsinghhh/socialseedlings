385cb180be367713b326deeb4ddf93dfdd6c7491
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-31 05:00:33 +0530

    navbar mode

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,31 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import logo from "../../images/logo.svg";
 import styles from "./navbar.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import Link from "next/link";
+import { useAppContext } from "@/context/appContext";
 
 const Navbar = () => {
+  const {mode} = useAppContext()
   return (
     <div>
       <div className={styles.logoMain}>
         <Link href={"/"} className={styles.myLink}>
-          <div className={styles.mainLogo}>
+          <div className={mode==='dark'?`${styles.mainLogo}`:`${styles.mainLogo1}`}>
             <Image src={logo} alt="logo" className={styles.logo} />
             <div>SocialSeedlings</div>
           </div>
         </Link>
-        <div className={styles.midTagLine}>
+        <div className={mode==='dark'?`${styles.midTagLine}`:`${styles.midTagLine1}`}>
           Fostering Societal Change
           <div>
             <DarkModeToggle className={styles.toggle} />
           </div>
         </div>
-        <div className={styles.endTagLine}>
+        
+        <div className={mode==='dark'?`${styles.endTagLine}`:`${styles.endTagLine1}`}>
           Eat. Socialize. Sleep <br />
           xRepeat
         </div>
