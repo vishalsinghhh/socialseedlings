c3e8bb792c2336b452d0e7cf8e9e5d694e43563c
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-07-30 15:19:36 +0530

    background name

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -9,41 +9,52 @@ const UserInfo = (userInfo) => {
   console.log(userInfo);
   return (
     <div>
-    <BackgroundName name={userInfo?.user?.name}/>
-      <div className={styles.mainProfile}>
-        <Image
-          src={userInfo?.user?.profile_image?.large}
-          width={200}
-          height={200}
-          className={styles.profileImg}
-        />
-        <div className={styles.profileDetails}>
-          <div>{userInfo?.user?.username}</div>
-          <div className={styles.photosDetails}>
-            <div>{userInfo?.user?.total_likes} likes</div>
-            <div>{userInfo?.user?.total_photos} photos</div>
-            <div>{userInfo?.user?.total_collections} collections</div>
+      <BackgroundName name={userInfo?.user?.name} />
+      <div className={styles.profileSection}>
+        <div className={styles.mainProfile}>
+          <Image
+            src={userInfo?.user?.profile_image?.large}
+            width={200}
+            height={200}
+            className={styles.profileImg}
+          />
+          <div className={styles.profileDetails}>
+            <div>{userInfo?.user?.username}</div>
+            <div className={styles.photosDetails}>
+              <div>{userInfo?.user?.total_likes} likes</div>
+              <div>{userInfo?.user?.total_photos} photos</div>
+              <div>{userInfo?.user?.total_collections} collections</div>
+            </div>
+            <div>{userInfo?.user?.name}</div>
+            <div>{userInfo?.user?.location}</div>
+            <div>{userInfo?.user?.bio}</div>
+            <div className={styles.links}>
+              <div>
+                <a
+                  href={`https://www.instagram.com/${userInfo?.user?.social?.instagram_username}/`}
+                  target="_"
+                >
+                  <BsInstagram />
+                </a>
+              </div>
+              <div>
+                <a
+                  href={`https://twitter.com/${userInfo?.user?.social?.twitter_username}`}
+                  target="_"
+                >
+                  <RxTwitterLogo />
+                </a>
+              </div>
+              <div>
+                <a href={userInfo?.user?.social?.portfolio_url} target="_">
+                  <BsLink45Deg />
+                </a>
+              </div>
+            </div>
           </div>
-          <div>{userInfo?.user?.name}</div>
-          <div>{userInfo?.user?.location}</div>
-          <div>{userInfo?.user?.bio}</div>
-          <div className={styles.links}>
-            <div>
-              <a href={`https://www.instagram.com/${userInfo?.user?.social?.instagram_username}/`} target="_">
-                <BsInstagram />
-              </a>
-            </div>
-            <div>
-              <a href={`https://twitter.com/${userInfo?.user?.social?.twitter_username}`} target="_">
-                <RxTwitterLogo />
-              </a>
-            </div>
-            <div>
-              <a href={userInfo?.user?.social?.portfolio_url} target="_">
-                <BsLink45Deg />
-              </a>
-            </div>
-          </div>
+        </div>
+        <div>
+          <div className={styles.followBTN}>Follow</div>
         </div>
       </div>
     </div>
